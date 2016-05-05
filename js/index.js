56edcd67491d37154f264a5a1a6e0b90ab092394
Oscar Sauceda <OSauceda@users.noreply.github.com>
2016-05-05 16:05:47 -0600

    update may 5th
    icons changed to font awesome

--- js/index.js
+++ js/index.js
@@ -1,69 +1,3 @@
-function clearSearchBox(a) {
-	var elSearchBox = document.getElementById("search-box");
-	switch (a){
-		case "clearOnFocus":
-			if (elSearchBox.value=="Search.."){
-				elSearchBox.value="";
-			}
-			break;
-		case "resetOnBlur":
-			if (elSearchBox.value.length==0){
-				elSearchBox.value="Search...";
-			}
-			break;
-		}
-}
-
-function validateEmptyInputFields(element,event) {
-	switch (event){
-		case "resetOnBlur":
-			switch(element){
-				case "inputName":
-					var element = document.getElementById("input-name");
-					if (element.value.length==0){
-						element.value="Your Name..";
-					}
-					break;
-				case "inputEmail":
-					var element = document.getElementById("input-email");
-					if (element.value.length==0){
-						element.value="Your Email..";
-					}
-					break;
-				case "inputMessage":
-					var element = document.getElementById("input-message");
-					if (element.value.length==0){
-						element.value="Your Message..";
-					}
-					break;
-			}// switch element
-			break;
-		case "clearOnFocus":
-			switch(element){
-				case "inputName":
-					var element = document.getElementById("input-name");
-					if (element.value=="Your Name.."){
-						element.value="";
-					}
-					break;
-				case "inputEmail":
-					var element = document.getElementById("input-email");
-					if (element.value=="Your Email.."){
-						element.value="";
-					}
-					break;
-				case "inputMessage":
-					var element = document.getElementById("input-message");
-					if (element.value=="Your Message.."){
-						element.value="";
-					}
-					break;
-			} //switch element
-			break;
-	} //switch event
-
-} //validateEmptyInputFields
-
 //contact form validations
 function validateContactForm(){
 	var contactFormInputName = document.getElementById("input-name"),
@@ -71,14 +5,14 @@ function validateContactForm(){
 		contactFormInputMessage = document.getElementById("input-message");
 
 	//validate if the name field is empty
-	if (contactFormInputName.value.length==0 || contactFormInputName.value=="Your Name.."){
+	if (contactFormInputName.value.length==0){
 		//alert("Please provide your name.");
 		sweetAlert("Please provide your name.");
 		contactFormInputName.focus();
 		return false;
 	}
 	//validate if the email field is empty
-	if (contactFormInputEmail.value.length==0 || contactFormInputEmail.value=="Your Email.."){
+	if (contactFormInputEmail.value.length==0){
 		//alert("Please provide your Email.");
 		sweetAlert("Please provide your Email.");
 		contactFormInputEmail.focus();
@@ -94,7 +28,7 @@ function validateContactForm(){
 		return false;
 	}
 	//validate if the message field is empty
-	if (contactFormInputMessage.value.length==0 || contactFormInputMessage.value=="Your Message.."){
+	if (contactFormInputMessage.value.length==0){
 		//alert("Please provide a Message.");
 		sweetAlert("Please provide a Message.");
 		contactFormInputMessage.focus();
@@ -111,53 +45,13 @@ function changeSelected(e) {
 		galleryButtons[i].classList.remove("selected");
 	}
 	e.target.classList.add("selected");
-
 }
 
 var galleryButtonContainer = document.getElementById("btn-work-type-container");
 
-galleryButtonContainer.addEventListener('click',changeSelected,false);
+//galleryButtonContainer.addEventListener('click',changeSelected,false);
 
-//Listeners for search box
-var searchBox = document.getElementById("search-box");
+//Listener for contact button
+var contactForm = document.getElementById("contact-us-form");
 
-searchBox.addEventListener('blur',function(){
-	clearSearchBox('resetOnBlur');
-},false);
-
-searchBox.addEventListener('focus',function(){
-	clearSearchBox('clearOnFocus');
-},false);
-
-//Listeners for contact form
-var inputName = document.getElementById("input-name");
-var inputEmail = document.getElementById("input-email");
-var inputMessage = document.getElementById("input-message");
-
-inputName.addEventListener('focus',function(){
-	validateEmptyInputFields('inputName','clearOnFocus');
-},false);
-
-inputName.addEventListener('blur',function(){
-	validateEmptyInputFields('inputName','resetOnBlur');
-},false);
-
-inputEmail.addEventListener('focus',function(){
-	validateEmptyInputFields('inputEmail','clearOnFocus');
-},false);
-
-inputEmail.addEventListener('blur',function(){
-	validateEmptyInputFields('inputEmail','resetOnBlur');
-},false);
-
-inputMessage.addEventListener('focus',function(){
-	validateEmptyInputFields('inputMessage','clearOnFocus');
-},false);
-
-inputMessage.addEventListener('blur',function(){
-	validateEmptyInputFields('inputMessage','resetOnBlur');
-},false);
-
-//Listener for submit button
-var submitButton = document.getElementById("submit-send");
-submitButton.addEventListener("click",validateContactForm,false);
+//contactForm.addEventListener("submit",validateContactForm,false);
--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,47 @@
 //Toggles the class "selected"
 $(".btn-work-type").on("click",function(){
-	$(".btn-work-type").removeClass("selected");
+	var btnWorkType = $(".btn-work-type")
+	btnWorkType.removeClass("selected");
 	$(this).addClass("selected");
+
+	if ($(this).index()==btnWorkType[0]){
+		$(".gallery-element").fadeIn(500);
+	} else {
+		$(".gallery-element").fadeOut(500);	
+	}
+});
+
+//contact form validations
+$("#contact-us-form").on("submit",function(){
+	var userName =$("#input-name"),
+		userEmail=$("#input-email"),
+		userMessage=$("#input-message"),
+		emailRegEx=/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+	//validation for empty name
+	if (userName.val().length==0){
+		sweetAlert("Please provide a Name");
+		return false;
+	}
+	//validation for empty email
+	if (userEmail.val().length==0){
+		sweetAlert("Please provide an Email");
+		return false;
+	}	
+	//validation for valid email
+	if (!emailRegEx.test(userEmail.val())) {
+		sweetAlert("Please provide a valid Email");
+		return false;
+	}
+	//validation for empty message
+	if (userMessage.val().length==0){
+		sweetAlert("Please provide a Message");
+		return false;
+	}
+}); //contact form validation
+
+//header scroll button
+$(".btn-scroll").on("click",function(){
+	$(this).toggleClass("scrolled");
+	$("header").toggleClass("scrolled");
 });
