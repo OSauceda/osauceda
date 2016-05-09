9a65b32f2c520da315ec04d9e55f309ef604c28c
Oscar Sauceda <OSauceda@users.noreply.github.com>
2016-05-09 16:57:44 -0600

    Includes modal window project

---
+++ js/index.js
@@ -0,0 +1,123 @@
+//contact form validations
+function validateContactForm(){
+	var contactFormInputName = document.getElementById("input-name"),
+		contactFormInputEmail = document.getElementById("input-email"),
+		contactFormInputMessage = document.getElementById("input-message");
+
+	//validate if the name field is empty
+	if (contactFormInputName.value.length==0){
+		//alert("Please provide your name.");
+		sweetAlert("Please provide your name.");
+		contactFormInputName.focus();
+		return false;
+	}
+	//validate if the email field is empty
+	if (contactFormInputEmail.value.length==0){
+		//alert("Please provide your Email.");
+		sweetAlert("Please provide your Email.");
+		contactFormInputEmail.focus();
+		return false;
+	}	
+
+	//validate if a valid email was provided
+	var emailPattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+	if (!emailPattern.test(contactFormInputEmail.value)){
+		//alert("Please provide a valid email address");
+		sweetAlert("Please provide a valid email address");
+		contactFormInputEmail.focus();
+		return false;
+	}
+	//validate if the message field is empty
+	if (contactFormInputMessage.value.length==0){
+		//alert("Please provide a Message.");
+		sweetAlert("Please provide a Message.");
+		contactFormInputMessage.focus();
+		return false;
+	}
+} //validate contact form
+
+//change selected item
+function changeSelected(e) {
+	var galleryButtons = document.getElementsByClassName("btn-work-type"),
+		galleryElements = document.getElementsByClassName("gallery-element"),
+		clickedButton = e.target;
+
+	for (var i = 0; i <= galleryButtons.length-1; i++) {
+		galleryButtons[i].classList.remove("selected");
+	}
+	e.target.classList.add("selected");
+
+	//hide gallery elements
+	for (var i = 0 ; i <= galleryElements.length-1; i++) {
+		if (e.target.id !== "photography-btn") {
+			galleryElements[i].classList.add("display-hidden");
+		} else {
+			galleryElements[i].classList.remove("display-hidden");
+		}
+	}
+	//hide gallery elements gradually
+	// for (var i = 0; i <= galleryElements.length-1; i++) {
+	// 	if (e.target.id !== "photography-btn") {
+	// 		for (var ii = 1; ii <= 10; ii++) {
+	// 			var op= 1-parseFloat('0.'+ii);
+	// 			if (ii==10){
+	// 				op = 0;
+	// 			}
+	// 			galleryElements[i].style.opacity = op;
+	// 		}
+	// 	} else {
+	// 		//galleryElements[i].classList.remove("display-hidden");
+	// 	}
+	// }
+}
+
+//scroll down header
+var scrollDownHeader = function(){
+	var scrollHeaderButton = document.getElementById("header-scroll-btn"),
+		headerContainer = document.getElementById("prueba");
+
+		headerContainer.classList.toggle("scrolled");
+		scrollHeaderButton.classList.toggle("scrolled");
+}
+
+//modal window object
+var modalWindow = function(width,height,title,message) {
+	this.width = width;
+	this.height = height;
+	this.title = title;
+	this.message = message;
+	this.popUpContainer = document.createElement("div");
+	this.popUpContainer.className="popUpContainer";
+	this.popUpWindow = document.createElement("div");
+	this.popUpWindow.className="popUpWindow";
+	this.closeButton = document.createElement("");
+	this.titleTag = document.createElement("");
+	this.contentTag = document.createElement("");
+};
+
+modalWindow.prototype.openWindow = function() {
+	document.body.appendChild(this.popUpContainer);
+};
+
+//Listener for header scroll button
+var headerScrollButton = document.getElementById("header-scroll-btn");
+
+headerScrollButton.addEventListener('click',scrollDownHeader,false);
+
+//Listener for lastest work galleries
+var galleryButtonContainer = document.getElementById("btn-work-type-container");
+
+galleryButtonContainer.addEventListener('click',changeSelected,false);
+
+//Listener for contact button
+var contactForm = document.getElementById("contact-us-form");
+
+contactForm.addEventListener("submit",validateContactForm,false);
+
+//Listener for popUp test button
+var popUpbtn = document.getElementById("pruebaPopUp");
+
+popUpbtn.addEventListener("click",function(e){
+	var infoPopUp = new modalWindow();
+	infoPopUp.openWindow();
+},false);
---
+++ js/jquery.js
@@ -0,0 +1,51 @@
+//Toggles the class "selected"
+$(".btn-work-type").on("click",function(){
+	var btnWorkType = $(".btn-work-type")
+	btnWorkType.removeClass("selected");
+	$(this).addClass("selected");
+
+	if ($(this).index()==btnWorkType[0]){
+		$(".gallery-element").fadeIn(1000);
+	} else {
+		$(".gallery-element").fadeOut(1000);	
+	}
+});//end Toggles the class "selected"
+
+//contact form validations
+$("#contact-us-form").on("submit",function(){
+	var userName =$("#input-name"),
+		userEmail=$("#input-email"),
+		userMessage=$("#input-message"),
+		emailRegEx=/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+	//validation for empty name
+	if (userName.val().length==0){
+		alert("Please provide a Name.");
+		userName.focus();
+		return false;
+	}
+	//validation for empty email
+	if (userEmail.val().length==0){
+		alert("Please provide an Email.");
+		userEmail.focus();
+		return false;
+	}	
+	//validation for valid email
+	if (!emailRegEx.test(userEmail.val())) {
+		alert("Please provide a valid Email.");
+		userEmail.focus();
+		return false;
+	}
+	//validation for empty message
+	if (userMessage.val().length==0){
+		alert("Please provide a Message.");
+		userMessage.focus();
+		return false;
+	}
+}); //end of contact form validation
+
+//header scroll button
+$(".btn-scroll").on("click",function(){
+	$(this).toggleClass("scrolled");
+	$("header").toggleClass("scrolled");
+});//header scroll button
